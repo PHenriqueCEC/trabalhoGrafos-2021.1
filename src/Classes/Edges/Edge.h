2fce3640b84fec200814902c0c94c0270c538bcc
Pedro Henrique <pedroalmeidavrb@gmail.com>
2021-06-21 18:35:17 -0300

    Criando as primeiras classes

---
+++ src/Classes/Edges/Edge.h
@@ -0,0 +1,20 @@
+#ifndef EDGE_H_INCLUDED
+#define EDGE_H_INCLUDED
+
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+class Edge {
+
+    public:
+
+    private:
+
+};
+
+#endif
