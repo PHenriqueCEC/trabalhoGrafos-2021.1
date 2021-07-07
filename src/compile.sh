7e4b1c4220bafb1d83253418eecc52178c04f737
Pedro Henrique <pedroalmeidavrb@gmail.com>
2021-07-07 17:25:28 -0300

    Colocando mais um arquivo sh

--- src/compile.sh
+++ src/compile.sh
@@ -1,6 +1,6 @@
 echo "Começando a compilação do projeto"
 
-cd ./Classes && g++ ./FileHandler/FileHandler.cpp ./Aresta/Aresta.cpp ./Grafo/Grafo.cpp ./No/No.cpp ../main.cpp -o ../execGrupo1
+ g++ *.c* -o execGrupo1
 if [ $? -eq 0 ];then 
    echo "compilado com sucesso!"
 else
--- src/main.cpp
+++ src/main.cpp
@@ -11,12 +11,7 @@ int main(int argc, char **argv)
 
   if(argc != 6)
   {
-    cout << argv[1] << endl;
-    cout << argv[2] << endl;
-    cout << argv[3] << endl;
-    cout << argv[4] << endl;
-    cout << argv[5] << endl;
-    cout << argv[6] << endl;
+    cout << argc << endl;
     cout << "entrei aqui" << argc << endl;
     cout << "ERRO! Verifique se os parametros foram passados corretamente!" << endl;
   }
